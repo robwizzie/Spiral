4bbaab7b2297f5a85b05178fd16af83ec65e1aa2
robwizzie <robertwiscount@gmail.com>
2025-11-04 18:20:50 -0500

    Initial Commit

---
+++ SpiralTests/SpiralTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpiralTests.swift
+//  SpiralTests
+//
+//  Created by Robert Wiscount on 11/4/25.
+//
+
+import Testing
+@testable import Spiral
+
+struct SpiralTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
